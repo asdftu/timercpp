eb99fe1941aaff4952b4e9e6bcca8725ec7e6db4
Logan <asdftu@gmail.com>
2021-11-09 11:09:31 +0800

    function template, static field version timer

---
+++ timer.h
@@ -0,0 +1,70 @@
+#ifndef TIMER_BOOST_H
+#define TIMER_BOOST_H
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <atomic>
+
+class Timer {
+    static uint64_t index;
+    static std::map<uint64_t, bool> active_map;
+    static std::mutex mutex;
+
+public:
+    template<typename Function>
+    uint64_t setTimeout(Function function, int delay);
+    template<typename Function>
+    uint64_t setInterval(Function function, int interval);
+    void stop(const uint64_t& id);
+};
+
+uint64_t Timer::index = 100;
+std::map<uint64_t,bool> Timer::active_map;
+std::mutex Timer::mutex;
+
+
+template<typename Function>
+uint64_t Timer::setTimeout(Function function, int delay) {
+    std::lock_guard<std::mutex> lock(mutex);
+    const uint64_t id = index;
+    active_map.insert(make_pair(id, true));
+    index++;
+    auto& active = active_map[id];
+    std::thread t([function, delay, &active]() {
+        if (!active.load()) return;
+        std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+        if (!active.load()) return;
+        function();
+        });
+    t.detach();
+    return id;
+}
+
+template<typename Function>
+uint64_t Timer::setInterval(Function function, int interval) {
+    std::lock_guard<std::mutex> lock(mutex);
+    const uint64_t id = index;
+    active_map.insert(make_pair(id, true));
+    index++;
+    auto& active = active_map[id];
+    std::thread t([function, interval, &active]() {
+        while (active.load()) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+            if (!active.load()) return;
+            function();
+        }
+        });
+    t.detach();
+    return id;
+}
+
+void Timer::stop(const uint64_t& id) {
+    std::lock_guard<std::mutex> lock(mutex);
+    if (active_map.find(id) != active_map.end())
+    {
+        active_map[id] = false;
+    }
+}
+
+
+#endif // TIMER_BOOST_H
